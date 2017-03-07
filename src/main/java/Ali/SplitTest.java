ae29b22523a4cb158bcb0bcadf9a2eff9f95a708
wangkang <wangkang93624@hotmail.com>
2017-03-07 18:43:15 +0800

    add readme

--- src/main/java/Ali/SplitTest.java
+++ src/main/java/Ali/SplitTest.java
@@ -8,45 +8,46 @@ import java.util.List;
  */
 public class SplitTest {
 
-    interface Test{
+    interface Test {
         void pring();
     }
-    public static boolean judge(int[] A){
-        List<Test> f=new ArrayList<>();
-        if (A.length<7){
+
+    public static boolean judge(int[] A) {
+        List<Test> f = new ArrayList<>();
+        if (A.length < 7) {
             return false;
         }
-        int i=0;
-        int j=A.length-1;
-        int sum1=A[i];
-        int sum4=A[j];
-        int sum2=0;
-        int sum3=0;
+        int i = 0;
+        int j = A.length - 1;
+        int sum1 = A[i];
+        int sum4 = A[j];
+        int sum2 = 0;
+        int sum3 = 0;
 
-        while (j-i>3){
-            if (sum1<sum4){
+        while (j - i > 3) {
+            if (sum1 < sum4) {
                 i++;
-                sum1+=A[i];
-            }else if (sum1>sum4){
+                sum1 += A[i];
+            } else if (sum1 > sum4) {
                 j--;
-                sum4+=A[j];
-            }else {
-                i+=2;
-                j-=2;
+                sum4 += A[j];
+            } else {
+                i += 2;
+                j -= 2;
 //                int a=i+2;
 //                int b=j-2;
-                sum2=A[i];
-                while (i<j+1){
-                    if (sum2!=sum1){
+                sum2 = A[i];
+                while (i < j + 1) {
+                    if (sum2 != sum1) {
                         i++;
-                        sum2+=A[i];
-                    }else {
-                        i+=2;
-                        while (i<=j){
-                            sum3+=A[i];
+                        sum2 += A[i];
+                    } else {
+                        i += 2;
+                        while (i <= j) {
+                            sum3 += A[i];
                             i++;
                         }
-                        if (sum3==sum2){
+                        if (sum3 == sum2) {
                             return true;
                         }
                     }
@@ -55,8 +56,11 @@ public class SplitTest {
         }
         return false;
     }
-    public static void main(String[] args){
-        int[] A=new int[]{10, 2, 11, 13, 1, 1, 1, 1, 1};
+
+    public static void main(String[] args) {
+        int[] A = new int[]{10, 2, 11, 13, 1, 1, 1, 1, 1};
         System.out.println(judge(A));
+        String a = "temp";
+        a.intern();
     }
 }
---
+++ src/main/java/javalang/string/StringInternTest.java
@@ -0,0 +1,18 @@
+package javalang.string;
+
+/**
+ * Created by wa on 2017/3/7.
+ */
+public class StringInternTest {
+    public static void main(String[] args) {
+        String s = new String("1");
+        s.intern();
+        String s2 = "1";
+        System.out.println(s == s2);
+
+        String s3 = new String("1") + new String("1");
+        s3.intern();
+        String s4 = "11";
+        System.out.println(s3 == s4);
+    }
+}
